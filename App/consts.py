c76b0a0dc643ea4c5978ac9e58dcd36fe991a5c4
Bill Sun <billsun@Bills-MacBook-Air.local>
2023-07-08 21:37:19 -0400

    init

---
+++ App/consts.py
@@ -0,0 +1,11 @@
+""" Define constants and paths as global variables """
+
+import os
+
+# Constants
+
+
+# Paths
+STATIC_PATH = os.path.join('App', 'static')
+SCHEMAS_PATH = os.path.join(STATIC_PATH, 'schemas')
+SURVEYS_PATH = os.path.join(STATIC_PATH, 'surveys')
