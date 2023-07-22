cc407301803990c9efca85dbd21bb2a4227ec268
Bill Sun <billsun@Bills-MacBook-Air.local>
2023-07-21 22:27:34 -0400

    more renaming

--- App/consts.py
+++ App/consts.py
@@ -8,7 +8,7 @@ import os
 # Paths
 STATIC_PATH = os.path.join('App', 'static')
 SCHEMAS_PATH = os.path.join(STATIC_PATH, 'schemas')
-SURVEYS_PATH = os.path.join(STATIC_PATH, 'surveys')
+SURVEYS_PATH = os.path.join(STATIC_PATH, 'surveys_files')
 METADATA_SCHEMA_PATH = os.path.join(SCHEMAS_PATH, 'metadata.json')
 QUESTIONS_SCHEMA_PATH = os.path.join(SCHEMAS_PATH, 'questions.json')
 RESULTS_SCHEMA_PATH = os.path.join(SCHEMAS_PATH, 'results.json')
